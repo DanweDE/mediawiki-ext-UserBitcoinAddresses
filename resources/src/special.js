4c2b8274e60718ff3a15ce88a5404c6db150aba1
Daniel A. R. Werner <daniel.a.r.werner@gmail.com>
2015-05-19 03:31:58 +0200

    Hide special page files if not required

--- resources/src/special.js
+++ resources/src/special.js
@@ -7,4 +7,15 @@
 ( function( $ ) {
 	'use strict';
 
+	$( document ).ready( function() {
+		var $addr1 = $( '#mw-input-wpaddresses' ); // valid addresses
+		var $addr2 = $( '#mw-input-wpaddressesToBeCorrected' ); // invalid addresses
+
+		if( $addr2.text() === '' ) {
+			$addr2.hide();
+		} else if( $addr1.text() === '' ) {
+			$addr1.hide();
+		}
+	} );
+
 } )( jQuery );
