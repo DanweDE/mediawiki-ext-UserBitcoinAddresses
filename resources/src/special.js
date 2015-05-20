90a92a899b9e6c9d9db4200b63b14cd8499f3bdd
Daniel A. R. Werner <daniel.a.r.werner@gmail.com>
2015-05-20 15:45:28 +0200

    restructured special page JS code and minor styling addition

--- resources/src/special.js
+++ resources/src/special.js
@@ -8,19 +8,28 @@
 	'use strict';
 
 	$( document ).ready( function() {
-		var $addr1 = $( '#mw-input-wpaddresses' ); // valid addresses
-		var $addr2 = $( '#mw-input-wpaddressesToBeCorrected' ); // invalid addresses
+		formatTextareas();
+		addFieldsetContainserrorClasses();
 
-		if( $addr2.text() === '' ) {
-			$addr2.hide();
-		} else if( $addr1.text() === '' ) {
-			$addr1.hide();
+		function formatTextareas() {
+			var $addr1 = $( '#mw-input-wpaddresses' ); // valid addresses
+			var $addr2 = $( '#mw-input-wpaddressesToBeCorrected' ); // invalid addresses
+
+			if( $addr2.text() === '' ) {
+				$addr2.hide();
+			} else if( $addr1.text() === '' ) {
+				$addr1.hide();
+			}
+
+			$addr1.add( $addr2 ).inputautoexpand( {
+				expandWidth: false,
+				expandHeight: true
+			} );
 		}
 
-		$addr1.add( $addr2 ).inputautoexpand( {
-			expandWidth: false,
-			expandHeight: true
-		} );
+		function addFieldsetContainserrorClasses() {
+			$( '#mw-content-text fieldset:has( .error )' ).addClass( 'containserror' );
+		}
 	} );
 
 } )( jQuery );
